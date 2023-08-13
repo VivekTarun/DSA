4f680f6aeaf90dd2d6a292abce09a10201ee6585
vivektarun <vivektarun1234@gmail.com>
2023-08-14 01:19:12 +0530

    palindrome Q added

--- Lecture022 Character Array and Strings/04palindrome.cpp
+++ Lecture022 Character Array and Strings/04palindrome.cpp
@@ -5,7 +5,7 @@ char toLowerCase(char ch) {
     if(ch >= 'a' && ch <= 'z')
     return ch;
     else {
-        char temp = ch - 'A' - 'a';
+        char temp = ch - 'A' + 'a';
         return temp;
     }
 
@@ -16,7 +16,7 @@ bool checkPalindrome(char a[], int n) {
     int e = n-1;
 
     while(s <= e) {
-        if(a[s] != a[e])
+        if( toLowerCase(a[s]) != toLowerCase(a[e]) )
         return 0;
         else {
             s++;
@@ -34,15 +34,47 @@ int getLength(char name[]) {
 
     return count;
 }
+
+char getMaxOccCharacter(string s) {
+    int arr[26] = {0};
+
+    for(int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+
+        //lowercase
+        int number = 0;
+        if(ch >= 'a' && ch <= 'z') {
+            number = ch - 'a';
+        } else {
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+
+    int maxi = -1, ans = 0;
+    for(int i = 0; i < 26; i++) {
+        if(maxi < arr[i]) {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+
+    char finalAns = 'a' + ans;
+    return finalAns;
+}
  
 int main()
 {
-    char name[20] = "vivek";
-    int lenght = getLength(name);
+    char name[20] = "Noon";
+    // int lenght = getLength(name);
 
-    if(checkPalindrome(name, lenght))
-    cout << "yes palindrome" << endl;
-    else
-    cout << "not palindrome" << endl;
-    return 0;
+    // if(checkPalindrome(name, lenght))
+    // cout << "yes palindrome" << endl;
+    // else
+    // cout << "not palindrome" << endl;
+    // return 0;
+
+    char letter = getMaxOccCharacter(name);
+
+    cout << letter << endl;
 }
