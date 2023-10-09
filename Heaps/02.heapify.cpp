38c59a438cf38b299f8aeb8cafa92839289450fc
vivektarun <vivektarun1234@gmail.com>
2023-10-10 01:20:27 +0530

    heap sort

--- Heaps/02.heapify.cpp
+++ Heaps/02.heapify.cpp
@@ -8,10 +8,10 @@ void heapify(int arr[], int n, int i) {
 
     int largest = index;
 
-    if(leftIndex < n && arr[largest] < arr[leftIndex]) {
+    if(leftIndex <= n && arr[largest] < arr[leftIndex]) {
         largest = leftIndex;
     }
-    if(rightIndex < n && arr[largest] < arr[rightIndex]) {
+    if(rightIndex <=    n && arr[largest] < arr[rightIndex]) {
         largest = rightIndex;
     }
 
@@ -29,9 +29,29 @@ void buildHeap(int arr[], int n) {
         heapify(arr, n, i);
     }
 }
+
+void heapSort(int arr[], int n ) {
+    int index = n;
+
+    while(n != 1) {
+        swap(arr[1], arr[index]);
+        n--;
+
+        //heapify
+        heapify(arr, n, index);
+    }
+}
  
 int main() {
+    int arr[] = {-1, 12, 15, 13, 11, 14};
+    int n = 5;
+
+    buildHeap(arr, n);
     
+    cout << "Pirnt the heap " << endl;
+    for(int i = 0; i<=n; i++) {
+        cout << arr[i] << " ";
+    } cout << endl;
     
     return 0;
 }
